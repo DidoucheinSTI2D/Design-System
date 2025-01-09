be2b74eeb1a71509c2b945967104df201a89230b
DidoucheInSTI2D <didouche78130@gmail.com>
2025-01-09 10:10:47 +0100

    🍃Add darkmode for Index and Forget and Fix css for profil

---
+++ darkmode.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const darkMode = document.querySelector("#darkMode");
+    const body = document.body;
+    const main = document.querySelector("main");
+
+    const actualTheme = localStorage.getItem("mode");
+
+    if (actualTheme === "dark") {
+        body.classList.add("body-dark");
+        main.classList.add("main-dark");
+        darkMode.checked = true;
+    }
+
+    darkMode.addEventListener("change", function() {
+        if (darkMode.checked) {
+            body.classList.add("body-dark");
+            main.classList.add("main-dark");
+            localStorage.setItem("mode", "dark");
+        } else {
+            body.classList.remove("body-dark");
+            main.classList.remove("main-dark");
+            localStorage.setItem("mode", "light");
+        }
+    })
+})
