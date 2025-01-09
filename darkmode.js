3cfab17ceb9eba2b44163667e9f587a7d9633ccb
Harvyn-10 <harvynguy@gmail.com>
2025-01-09 20:04:46 +0100

    svg commit

--- darkmode.js
+++ darkmode.js
@@ -26,4 +26,18 @@ document.addEventListener("DOMContentLoaded", function() {
             localStorage.setItem("mode", "light");
         }
     })
-})
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    const passwordInput = document.getElementById("password");
+    const eyePasswordIcon = document.getElementById("eyePasswordVisibility");
+  
+    eyePasswordIcon.addEventListener("click", () => {
+
+      const IsPasswordHidden = passwordInput.type === "password";
+      passwordInput.type = IsPasswordHidden ? "text" : "password";
+  
+      eyePasswordIcon.src = IsPasswordHidden ? "assets/eye-slash.svg" : "assets/eye.svg";
+    });
+  });
+  
