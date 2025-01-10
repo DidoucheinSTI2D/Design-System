62f11cad3f89537c8ca5be73cf5d1501e8870740
Harvyn-10 <harvynpillah206@gmail.com>
2025-01-10 11:11:42 +0100

    Error correction

---
+++ Darkmode.js
@@ -0,0 +1,35 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const darkMode = document.querySelector(".darkMode");
+    const body = document.body;
+    const main = document.querySelector("main");
+    const sidebar = document.querySelector(".sidebar");
+
+    const actualTheme = localStorage.getItem("mode");
+
+    if (actualTheme === "dark") {
+        body.classList.add("body-dark");
+        main.classList.add("main-dark");
+        if (sidebar) {
+            sidebar.classList.add("sidebar-dark");
+        }
+        darkMode.checked = true;
+    }
+
+    darkMode.addEventListener("change", function() {
+        if (darkMode.checked) {
+            body.classList.add("body-dark");
+            main.classList.add("main-dark");
+            if (sidebar) {
+                sidebar.classList.add("sidebar-dark");
+            }
+            localStorage.setItem("mode", "dark");
+        } else {
+            body.classList.remove("body-dark");
+            main.classList.remove("main-dark");
+            if (sidebar) {
+                sidebar.classList.remove("sidebar-dark");
+            }
+            localStorage.setItem("mode", "light");
+        }
+    })
+});
